163d90285a999dab2122270ef80e7249bcc14441
Olav Vitters <olav@vitters.nl>
2012-02-29 10:46:39 +0100

    initial import

---
+++ Config.pm
@@ -0,0 +1,5 @@
+package Bugzilla::Extension::StockAnswers;
+use strict;
+use constant NAME => 'StockAnswers';
+
+__PACKAGE__->NAME;
---
+++ Extension.pm
@@ -0,0 +1,11 @@
+package Bugzilla::Extension::StockAnswers;
+use strict;
+use base qw(Bugzilla::Extension);
+
+
+
+our $VERSION = '';
+
+
+
+__PACKAGE__->NAME;
